24a8f9940c543939b104d42b687019990914fabd
Hoa Nguyen <hoanguyen@ucdavis.edu>
2021-02-24 14:33:01 -0800

    Clean up
    Signed-off-by: Hoa Nguyen <hoanguyen@ucdavis.edu>

--- launch_tests.py
+++ launch_tests.py
@@ -1,6 +1,7 @@
 from itertools import product as cross_product
 import multiprocessing as mp
 import os
+import pathlib
 import sys
 import traceback
 
@@ -12,12 +13,22 @@ import input_space
 from gem5art.artifact.artifact import Artifact
 from gem5art.run import gem5Run
 
+
+ABS_PATH = pathlib.Path(__file__).parent.absolute()
+
 OUTPUT_FOLDER = "/projects/gem5/gem5-resources-20.1/"
-ERR_FOLDER = "/scr/hn/gem5-resources-launch/error_logs/"
+ERR_FOLDER = os.join.path(ABS_PATH, "error_logs/")
+GEM5_FOLDER = os.join.path(ABS_PATH, "gem5/")
+GEM5_RESOURCES_FOLDER = os.join.path(ABS_PATH, "gem5-resources/")
+DISK_IMAGES_FOLDER = os.join.path(ABS_PATH, "disk-images/")
+LINUX_KERNELS_FOLDER = os.join.path(ABS_PATH, "linux-kernels/")
 RUN_NAME_SUFFIX = "launched:02/23/2021;gem5art-status;v20.1.0.4;kvm;lavandula-angustifolia"
 
 def lists_to_dict(keys, vals):
     return dict(zip(keys, vals))
+
+def to_abs_path(path): # return the absoblute path of a relative path, assuming the relative path to be relative to the folder containing this script
+    return os.path.join(ABS_PATH, path)
 
 def get_boot_exit_jobs_iterator():
     name = 'boot-exit'
@@ -85,9 +96,9 @@ def get_jobs_iterator():
 
 def get_gem5_binary_path(mem_sys):
     if mem_sys == "classic":
-        return "/scr/hn/gem5-resources-launch/gem5/build/X86/gem5.opt"
+        return os.path.join(GEM5_FOLDER, "build/X86/gem5.opt")
     else:
-        return "/scr/hn/gem5-resources-launch/gem5/build/X86_{}/gem5.opt".format(mem_sys)
+        return os.path.join(GEM5_FOLDER, "build/X86_{}/gem5.opt".format(mem_sys))
 
 # https://github.com/darchr/gem5art-experiments/blob/master/launch-scripts/launch_boot_tests_gem5_20.py#L128
 def create_boot_exit_fs_run(params):
@@ -105,13 +116,13 @@ def create_boot_exit_fs_run(params):
     gem5run = gem5Run.createFSRun(
         'boot-exit;'+RUN_NAME_SUFFIX, # name
         get_gem5_binary_path(mem_sys), # gem5_binary
-        '/scr/hn/gem5-resources-launch/gem5-resources/src/boot-exit/configs/run_exit.py', # run_script
+        os.path.join(GEM5_RESOURCES_FOLDER, 'src/boot-exit/configs/run_exit.py'), # run_script
         os.path.join(OUTPUT_FOLDER, 'boot-exit/{}/{}/{}/{}/{}/'. format(kernel, cpu, num_cpu, mem_sys, boot_type)), # outdir
         gem5_binaries[mem_sys], # gem5_artifact
         gem5_repo, # gem5_git_artifact
         experiments_repo, # run_script_git_artifact
-        os.path.join('/scr/hn/gem5-resources-launch/linux-kernels', 'vmlinux'+'-'+kernel), # linux_binary
-        '/scr/hn/gem5-resources-launch/disk-images/boot-exit.img', # disk_image
+        os.path.join(LINUX_KERNELS_FOLDER, 'vmlinux'+'-'+kernel), # linux_binary
+        os.path.join(DISK_IMAGES_FOLDER, 'boot-exit.img'), # disk_image
         linux_binaries[kernel], # linux_binary_artifact
         boot_exit_artifacts.disk_image, # disk_image_artifact
         cpu, mem_sys, num_cpu, boot_type, # params
@@ -134,13 +145,13 @@ def create_npb_fs_run(params):
     gem5run = gem5Run.createFSRun(
             'npb;'+RUN_NAME_SUFFIX, # name
             get_gem5_binary_path(mem_sys), # gem5_binary
-            '/scr/hn/gem5-resources-launch/gem5-resources/src/npb/configs/run_npb.py', # run_script
+            os.path.join(GEM5_RESOURCES_FOLDER, 'src/npb/configs/run_npb.py'), # run_script
             os.path.join(OUTPUT_FOLDER, 'npb/{}/{}/{}/{}/{}/'. format(kernel, cpu, num_cpu, mem_sys, workload)), # outdir
             gem5_binaries[mem_sys], # gem5_artifact
             gem5_repo, # gem5_git_artifact
             experiments_repo, # run_script_git_artifact
-            os.path.join('/scr/hn/gem5-resources-launch/linux-kernels', 'vmlinux'+'-'+kernel), # linux_binary
-            '/scr/hn/gem5-resources-launch/disk-images/npb.img', # disk_image
+            os.path.join(LINUX_KERNELS_FOLDER, 'vmlinux'+'-'+kernel), # linux_binary
+            os.path.join(DISK_IMAGES_FOLDER, 'npb.img'), # disk_image
             linux_binaries[kernel], # linux_binary_artifact
             npb_artifacts.disk_image, # disk_image_artifact
             cpu, mem_sys, workload, num_cpu, # params
@@ -163,13 +174,13 @@ def create_gapbs_fs_run(params):
     gem5run = gem5Run.createFSRun(
         'gapbs;'+RUN_NAME_SUFFIX, # name
         get_gem5_binary_path(mem_sys), # gem5_binary
-        '/scr/hn/gem5-resources-launch/gem5-resources/src/gapbs/configs/run_gapbs.py', # run_script
+        os.path.join(GEM5_RESOURCES_FOLDER, 'src/gapbs/configs/run_gapbs.py'), # run_script
         os.path.join(OUTPUT_FOLDER, 'gapbs/{}/{}/{}/{}/{}/{}/{}/'. format(kernel, cpu, num_cpu, mem_sys, workload, synthetic, graph)), # outdir
         gem5_binaries[mem_sys], # gem5_artifact
         gem5_repo, # gem5_git_artifact
         experiments_repo, # run_script_git_artifact
-        os.path.join('/scr/hn/gem5-resources-launch/linux-kernels', 'vmlinux'+'-'+kernel), # linux_binary
-        '/scr/hn/gem5-resources-launch/disk-images/gapbs.img', # disk_image
+        os.path.join(LINUX_KERNELS_FOLDER, 'vmlinux'+'-'+kernel), # linux_binary
+        os.path.join(DISK_IMAGES_FOLDER, 'gapbs.img'), # disk_image
         linux_binaries[kernel], # linux_binary_artifact
         gapbs_artifacts.disk_image, # disk_image_artifact
         cpu, num_cpu, mem_sys, workload, synthetic, graph, # params
@@ -191,9 +202,9 @@ def create_parsec_fs_run(params):
         timeout = 10*24*60*60 # 10 days
 
     if mem_sys == "classic":
-        run_script = "/scr/hn/gem5-resources-launch/gem5-resources/src/parsec/configs/run_parsec.py"
+        run_script = os.path.join(GEM5_RESOURCES_FOLDER, "src/parsec/configs/run_parsec.py")
     else:
-        run_script = "/scr/hn/gem5-resources-launch/gem5-resources/src/parsec/configs-mesi-two-level/run_parsec_mesi_two_level.py"
+        run_script = os.path.join(GEM5_RESOURCES_FOLDER, "src/parsec/configs-mesi-two-level/run_parsec_mesi_two_level.py")
 
     gem5run = gem5Run.createFSRun(
         'parsec;'+RUN_NAME_SUFFIX, # name
@@ -203,8 +214,8 @@ def create_parsec_fs_run(params):
         gem5_binaries[mem_sys], # gem5_artifact
         gem5_repo, # gem5_git_artifact
         experiments_repo, # run_script_git_artifact
-        os.path.join('/scr/hn/gem5-resources-launch/linux-kernels', 'vmlinux'+'-'+kernel), # linux_binary
-        '/scr/hn/gem5-resources-launch/disk-images/parsec.img', # disk_image
+        os.path.join(LINUX_KERNELS_FOLDER, 'vmlinux'+'-'+kernel), # linux_binary
+        os.path.join(DISK_IMAGES_FOLDER, 'parsec.img'), # disk_image
         linux_binaries[kernel], # linux_binary_artifact
         parsec_artifacts.disk_image, # disk_image_artifact
         cpu, workload, size, num_cpu, # params
@@ -225,13 +236,13 @@ def create_spec_2006_fs_run(params):
     gem5run = gem5Run.createFSRun(
         'spec-2006;'+RUN_NAME_SUFFIX, # name
         get_gem5_binary_path('classic'), # gem5 binary
-        '/scr/hn/gem5-resources-launch/gem5-resources/src/spec-2006/configs/run_spec.py', # run_script
+        os.path.join(GEM5_RESOURCES_FOLDER, 'src/spec-2006/configs/run_spec.py'), # run_script
         os.path.join(OUTPUT_FOLDER, 'spec-2006/{}/{}/{}/{}/{}/'. format(kernel, cpu, mem_sys, workload, size)), # outdir
         gem5_binaries['classic'], # gem5_artifact
         gem5_repo, # gem5_git_artifact
         experiments_repo, # run_script_git_artifact
-        os.path.join('/scr/hn/gem5-resources-launch/linux-kernels', 'vmlinux'+'-'+kernel), # linux_binary
-        '/scr/hn/gem5-resources-launch/disk-images/spec-2006', # disk_image
+        os.path.join(LINUX_KERNELS_FOLDER, 'vmlinux'+'-'+kernel), # linux_binary
+        os.path.join(DISK_IMAGES_FOLDER, 'spec-2006'), # disk_image
         linux_binaries[kernel], # linux_binary_artifact
         spec_2006_artifacts.disk_image, # disk_image_artifact
         cpu, mem_sys, workload, size, # params
@@ -251,13 +262,13 @@ def create_spec_2017_fs_run(params):
     gem5run = gem5Run.createFSRun(
         'spec-2017;'+RUN_NAME_SUFFIX, # name
         get_gem5_binary_path('classic'), # gem5_binary
-        '/scr/hn/gem5-resources-launch/gem5-resources/src/spec-2017/configs/run_spec.py', # run_script
+        os.path.join(GEM5_RESOURCES_FOLDER, 'src/spec-2017/configs/run_spec.py'), # run_script
         os.path.join(OUTPUT_FOLDER, 'spec-2017/{}/{}/{}/{}/'. format(kernel, cpu, workload, size)), # outdir
         gem5_binaries['classic'], # gem5_artifact
         gem5_repo, # gem5_git_artifact
         experiments_repo, # run_script_git_artifact
-        os.path.join('/scr/hn/gem5-resources-launch/linux-kernels', 'vmlinux'+'-'+kernel), # linux_binary
-        '/scr/hn/gem5-resources-launch/disk-images/spec-2017', # disk_image
+        os.path.join(LINUX_KERNELS_FOLDER, 'vmlinux'+'-'+kernel), # linux_binary
+        os.path.join(DISK_IMAGES_FOLDER, 'spec-2017'), # disk_image
         linux_binaries[kernel], # linux_binary_artifact
         spec_2017_tests_artifacts.disk_image, # disk_image_artifact
         cpu, workload, size, # params
