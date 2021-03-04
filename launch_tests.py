8e3ae71c1982eadd3750c68191f8491ab5666e9d
Hoa Nguyen <hoanguyen@ucdavis.edu>
2021-03-04 07:54:49 -0800

    Run o3 and add packer script for parsec 20.04
    Signed-off-by: Hoa Nguyen <hoanguyen@ucdavis.edu>

--- launch_tests.py
+++ launch_tests.py
@@ -350,6 +350,6 @@ if __name__ == "__main__":
             f.write(str(job))
             f.write("\n")
     jobs = get_jobs_iterator()
-    #with mp.Pool(mp.cpu_count() // 2) as pool:
-    #    pool.map(worker, jobs)
+    with mp.Pool(mp.cpu_count() // 2) as pool:
+        pool.map(worker, jobs)
 
